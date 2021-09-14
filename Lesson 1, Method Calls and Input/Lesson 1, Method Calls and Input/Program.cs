8aba9d442df1d50b4199ab05b70f46ad81d7fdd2
Thomas-474 <teg2004@icloud.com>
2021-09-14 10:26:35 -0400

    Add project files.

---
+++ Lesson 1, Method Calls and Input/Lesson 1, Method Calls and Input/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1__Method_Calls_and_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
