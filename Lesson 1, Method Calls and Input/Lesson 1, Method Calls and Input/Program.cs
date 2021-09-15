5dafc4a4949bed371b521b1c12d57b2bfba0d188
Thomas-474 <teg2004@icloud.com>
2021-09-15 10:41:39 -0400

    Setting up Methods

--- Lesson 1, Method Calls and Input/Lesson 1, Method Calls and Input/Program.cs
+++ Lesson 1, Method Calls and Input/Lesson 1, Method Calls and Input/Program.cs
@@ -8,11 +8,48 @@ namespace Lesson_1__Method_Calls_and_Input
 {
     class Program
     {
+        static void NL()
+        {
+            Console.WriteLine("");
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Call a Method:")
+            Console.WriteLine("Call a Method:");
             CallAMethod();
-            NewLine
+            NL();
+
+            Console.WriteLine("Capture Output:");
+            CaptureOutput();
+            NL();
+
+            Console.WriteLine("Define a Method:");
+            //();
+            NL();
+
+            Console.WriteLine("Define Parameters:");
+            //();
+            NL();
+
+            Console.WriteLine("A Note on Parameters:");
+            //();
+            NL();
+
+            Console.WriteLine("Optional Parameters:");
+            //();
+            NL();
+
+            Console.WriteLine("Named Arguments:");
+            //();
+            NL();
+
+            Console.WriteLine("Method Overloading:");
+            //();
+            NL();
+
+            Console.WriteLine("Review:");
+            //();
+            NL();
 
             Console.ReadKey();
         }
@@ -24,6 +61,49 @@ namespace Lesson_1__Method_Calls_and_Input
             string msg = "Yabba dabba doo!";
             Console.WriteLine(msg);
             Console.WriteLine(msg.Substring(0, 1));
+        }
+
+        static void CaptureOutput()
+        {
+            string designer = "Anders Hejlsberg";
+            int indexOfSpace = designer.IndexOf(" ");
+            string secondName = designer.Substring(indexOfSpace);
+            Console.WriteLine(secondName);
+        }
+
+        static void DefineAMethod()
+        {
+
+        }
+
+        static void DefineParameters()
+        {
+
+        }
+
+        static void ANoteOnParameters()
+        {
+
+        }
+
+        static void OptionalParameters()
+        {
+
+        }
+
+        static void NamedArguments()
+        {
+
+        }
+
+        static void MethodOverloading()
+        {
+
+        }
+
+        static void Review()
+        {
+
         }
 
     }
